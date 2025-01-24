85d7cc382351fce7602caf7a7efb8a6081b53cb0
RaulBirlem <raul.birlem@gmail.com>
2025-01-23 23:35:05 -0300

    updateTask

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -12,6 +12,7 @@ import Modal from "./components/Modal"
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
   const deleteTask = (id:number) => {
     setTaskList(
@@ -28,13 +29,25 @@ function App() {
     }
   }
 
-  const editTask = ():void => {
+  const editTask = (task:ITask):void => {
     hideOrShowModal(true);
+    setTaskToUpdate(task);
+  }
+
+
+  const updateTask = (id: number, title:string, difficulty:number)=> {
+    const updatedTask: ITask = {id, title, difficulty}
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    })
+    setTaskList(updatedItems);
+    hideOrShowModal(false);
   }
 
   return (
     <>
-    <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList}/>} />
+    <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} task={taskToUpdate} handleUpdate={updateTask}/>}/>
       <Header />
       <main className={styles.main}>
       <div>
